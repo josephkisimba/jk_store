dfef39579ee7d369bce8f797e2a61b331ce4ef95
josephkisimba <josephkisimba07@gmail.com>
2021-03-31 11:31:03 +0200

    add state and mutation in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        cart: [],
+    },
+    mutations: {
+        addToCart(state, product) {
+            const item = state.cart.find((crt) => crt.product === product);
+            if (!item) {
+                state.cart.push(product);
+            } else {
+                item.product++;
+            }
+
+            // state.cart.push(product);
+        },
+    },
+    removeToCart() {
+        // state.cart.splice();
+    },
+    actions: {},
+});
