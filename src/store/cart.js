c869dd0135e4bf86f32bcc12008e6afbcabe7288
josephkisimba <josephkisimba07@gmail.com>
2021-06-01 11:47:09 +0200

    work on store

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,4 @@
 export default {
-    // namespacd: true,
     state: () => ({
         cart: [],
     }),
@@ -34,9 +33,8 @@ export default {
                 item.quantity += 1;
             }
         },
+
         decrement(state, id) {
-            // console.log("Search existing product in store");
-            // let findProduct = state.products.find((data) => data.id === product.id);
             const item = state.cart.find((data) => data.id === id);
 
             if (item && item.quantity > 1) {
@@ -50,57 +48,5 @@ export default {
                 return itemCart.id !== id;
             });
         },
-        // setUser(state, user) {
-        //     state.user = user;
-        //     console.log("setUser", user);
-        // },
-        // logOutUser(state) {
-        //     state.user = null;
-        // },
-        // addToCart(state, product) {
-        //     const item = state.cart.find((data) => data.id === product.id);
-        //     if (!item) {
-        //         product.quantity = 1;
-        //         state.cart.push(product);
-        //         // console.log("products added", product);
-        //     } else {
-        //         item.quantity += 1;
-        //     }
-        // },
-        // increment(state, id) {
-        //     const item = state.cart.find((data) => data.id === id);
-        //     const itemIndex = state.cart.cart.findIndex((data) => data.id === id);
-        //     // console.log("item", item);
-        //     // console.log("itemIndex", itemIndex);
-        //     if (item && itemIndex >= 0) {
-        //         console.log("increment");
-        //         item.quantity += 1;
-        //         state.cart[itemIndex] = item;
-        //         state.cart = state.cart;
-        //         // console.log("cart", state.cart);
-        //     }
-        // },
-        // decrement(state, id) {
-        //     const item = state.cart.find((data) => data.id === id);
-        //     const itemIndex = state.cart.cart.findIndex((data) => data.id === id);
-        //     // console.log("item", item);
-        //     // console.log("itemIndex", itemIndex);
-        //     if (item && itemIndex >= 0) {
-        //         console.log("decrement");
-        //         item.quantity -= 1;
-        //         if (item.quantity >= 1) {
-        //             state.cart[itemIndex] = item;
-        //         } else {
-        //             state.cart.splice(itemIndex, 1);
-        //         }
-        //         state.cart = state.cart;
-        //         // console.log("cart", state.cart);
-        //     }
-        // },
-        // removeFromCart(state, id) {
-        //     state.cart = state.cart.filter((itemCart) => {
-        //         return itemCart.id !== id;
-        //     });
-        // },
     },
 };
