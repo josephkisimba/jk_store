fa85648577f4f16c2987ade833b51e535c074a66
josephkisimba <josephkisimba07@gmail.com>
2021-07-22 11:16:04 +0200

    deploy

---
+++ js/chunk-2d21a3d2.3e703e7a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an home page")])])}],c=t("2877"),u={},i=Object(c["a"])(u,a,s,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.3e703e7a.js.map
